7ac23c4585598db3ea09a3933ad952f433217197
lucasaas98 <lucasaquilino@live.com.pt>
2023-10-10 20:23:15 +0000

    feat: deal with shorts, smaller player

--- frontend/frontend/utils.py
+++ frontend/frontend/utils.py
@@ -87,4 +87,5 @@ def prepare_for_template(video):
         "channel": video.channel,
         "progress_percentage": progress_percentage(video),
         "size": format_video_size(video),
+        "player_width": 25 if video.short else 80,
     }
