6c5b2c4346d38e47c810fd5a5bf1e8f8cec67861
lucasaas98 <lucasaquilino@live.com.pt>
2023-10-10 20:27:01 +0000

    format

--- frontend/frontend/utils.py
+++ frontend/frontend/utils.py
@@ -86,5 +86,5 @@ def prepare_for_template(video):
         "description": video.description,
         "channel": video.channel,
         "progress_percentage": progress_percentage(video),
-        "size": format_video_size(video)
+        "size": format_video_size(video),
     }
